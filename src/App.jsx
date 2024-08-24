5cf94e60f392fd19942be609e1688fef89c647d8
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-08-24 16:30:13 -0700

    Initial timer that counts down from 30 seconds, can be stopped, started and reset.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
+import { useState, useEffect } from "react";
+
 import './App.css'
 
 function App() {
+  const [seconds, setSeconds] = useState(30);
+  const [timerState, setTimerState] = useState("stopped");
+
+  //
+  useEffect(() => {
+    let interval;
+
+    if (timerState === "running" && seconds) {
+      interval = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds - 1);
+      }, 1000)
+    };
+
+    return () => clearInterval(interval);
+  }, [timerState, seconds]);
+
+  //
+  const startOrStopTimer = () => {
+    if (seconds === 0) {
+      resetTimer();
+      return setTimerState("running");
+    } else if (timerState === "stopped") {
+      return setTimerState("running");
+    } else {
+      setTimerState("stopped");
+    };
+    setTimerState("stopped");
+  };
+
+  const resetTimer = () => {
+    setTimerState("stopped");
+    setSeconds(30);
+  };
 
   return (
-    <></>
+    <>
+      <header>Circuit Timer</header>
+      <main>
+        <div className="individual-timer-component">
+          <div>{seconds}</div>
+          <button onClick={startOrStopTimer}>{timerState !== "running" ? "Start" : "Stop"}</button>
+          <button onClick={resetTimer}>Reset</button>
+        </div>
+      </main>
+    </>
   )
-}
+};
 
-export default App
+export default App;
