536e70a5ccc617d845c05705b2da56de9bb3f844
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-09-28 12:13:45 -0700

    Header tells you to press start if timer is stopped, or indicates if you should work or rest.

--- src/App.jsx
+++ src/App.jsx
@@ -233,7 +233,7 @@ function App() {
           ) : (
             // timer page
             <div className="timer-page-container">
-              <header className="glow">{roundPeriod === "workout" ? "Work" : "Rest"}</header>
+              <header className="glow">{timerState === "stopped" ? "Press Start" : roundPeriod === "workout" ? "Work" : "Rest"}</header>
               <div className="space-around-container workout-status">
                 <span>Rounds Completed: <strong>{roundsCompleted}</strong></span>
                 <span>Rounds to Go: <strong>{rounds - roundsCompleted}</strong></span>  
